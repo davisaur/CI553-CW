ca64dcd590ab322f1b582197f42742986e6ec6ee
davi <39279817+davisaur@users.noreply.github.com>
2024-01-10 20:59:55 +0000

    Added quantity merge and sort

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -12,7 +12,18 @@ import java.util.Collections;
 public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
-  
 
   // You need to add code here
+  @Override
+  public boolean add(Product pr) {
+    for (Product product : this) {
+      if (product.getProductNum().equals(pr.getProductNum())) {
+        int newQuantity = product.getQuantity() + 1;
+        product.setQuantity(newQuantity);
+        return true;
+      }
+    }
+    return super.add(pr);
+  }
 }
+
--- catalogue/BetterBasketTest.java
+++ catalogue/BetterBasketTest.java
@@ -5,8 +5,5 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BetterBasketTest {
-    @Test
-    void testAddProduct() {
-        fail("Not implemented yet");
-    }
+
 }
