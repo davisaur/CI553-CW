3ae34dc3423d1814883283012baa1f555aadef99
davi <39279817+davisaur@users.noreply.github.com>
2024-01-10 19:35:30 +0000

    Unit Testing init

---
+++ catalogue/BetterBasketTest.java
@@ -0,0 +1,12 @@
+package catalogue;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BetterBasketTest {
+    @Test
+    void testAddProduct() {
+        fail("Not implemented yet");
+    }
+}
