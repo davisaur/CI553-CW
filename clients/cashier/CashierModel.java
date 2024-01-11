4c581490f61e5f02d2606ab41c55f1d579003fd8
davi <39279817+davisaur@users.noreply.github.com>
2024-01-11 00:43:39 +0000

    Working Cashier VOID button

--- clients/cashier/CashierModel.java
+++ clients/cashier/CashierModel.java
@@ -131,10 +131,51 @@ public class CashierModel extends Observable
     setChanged(); notifyObservers(theAction);
   }
 
+  /**
+ * Voids the sale of a product and replenishes stock if the product is in the basket.
+ */
   public void doVoid() {
-    // TODO: Add code to VOID items
+    String theAction = "";
+    int    amount  = 1;
+    if( theState != State.checked ){ // If has not been checked with customer
+      theAction = "Check the item with customer first"; // Alert User
+    } else {
+      if (theBasket == null){ // Otherwise, check if basket doesn't exists
+        theAction = "The Basket is Empty."; // If it doesn't, alert user
+    } else {
+        int indexToRemove = -1;  // initiate index
+        for (int i = 0; i < theBasket.size(); i++) // check all products within basket
+        {
+          if (theBasket.get(i).getProductNum().equals(theProduct.getProductNum())) // if product found in basket
+          {
+            indexToRemove = i; // set indexToRemove to the index or the product in basket
+            break; // break out of loop
+          } // else keep repeating until gone through entire basket
+        }
+
+        if (indexToRemove != -1) {  // if a product has been found
+          try {
+            theStock.addStock(theProduct.getProductNum(), theProduct.getQuantity()); // add back the stock of the product
+            if(theBasket.get(indexToRemove).getQuantity() > 1) { // if there's more than one of the product
+              theBasket.get(indexToRemove).setQuantity(theBasket.get(indexToRemove).getQuantity() - 1); // reduce the quantity by 1
+            } else {
+              theBasket.remove(indexToRemove); // else remove the entire product from the basket
+            }
+            theAction = "Removed " +            //    tell user product was removed
+                    theProduct.getDescription();  //
+          } catch (StockException e) {
+            DEBUG.error("CashierModel.doVoid() - %s", e.getMessage());
+          }
+        } else { // if the product has NOT been found
+          theAction = "!!! Item is NOT in basket";       // alert user saying it's not in the basket
+        }
+      }
+    }
+    theState = State.process;                   // All Done
+    setChanged(); notifyObservers(theAction);
   }
-  
+
+
   /**
    * Customer pays for the contents of the basket
    */
